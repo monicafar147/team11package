2bc700f9aa130869f6ecab40870d34615337db1e
monicafar147 <monicafar147@gmail.com>
2020-02-14 10:30:57 +0200

    Update team11Module.py
    1. changed values of mean and median for the python module to run, Courtney please check that I didnt overwrite any of your code
    2. Added code to stopwords remover

--- team11package/team11Module.py
+++ team11package/team11Module.py
@@ -1,3 +1,7 @@
+#import relevant modules
+import numpy as np
+import pandas as pd
+
 def dictionary_of_metrics(items):
     """Return a dictionary of statistical measures: mean, median, variance, standard deviation, minimum and maximum of a list of items.
     All values in the returned dict will be rounded to 2 decimal places.
@@ -16,17 +20,8 @@ def dictionary_of_metrics(items):
                                    'min': 8842.00,
                                    'max': 39660.00}
     """
-    # Use numpy to find the metrics: Courtney
-        mean = round(np.mean(listofints), 2)
-        median = round(np.median(listofints),2)
-        var = round(np.var(listofints), 2)
-        std = round(np.std(listofints),2)
-        min = round(np.min(listofints),2)
-        max = round(np.max(listofints),2)
-
-
-    mean = 0
-    median = 0
+    ourmean = 0
+    ourmedian = 0
     variance = 0
     standard_dev = 0
     minimum = 0
@@ -35,8 +30,8 @@ def dictionary_of_metrics(items):
     # Create and return dictionary: Mikael
     dict = {}
 
-    dict['mean'] = mean
-    dict['median'] = median
+    dict['mean'] = ourmean
+    dict['median'] = ourmedian
     dict['variance'] = variance
     dict['standard variance'] = standard_dev
     dict['min'] = minimum
@@ -249,8 +244,51 @@ def stop_words_remover(df):
 
     Expected output should be same dataframe but with new column heading Without Stop Words
     """
+    #create stop words dictionary
+    # dictionary of english stopwords
+    stop_words_dict = {
+    'stopwords':[
+        'where', 'done', 'if', 'before', 'll', 'very', 'keep', 'something', 'nothing', 'thereupon', 
+        'may', 'why', 'â€™s', 'therefore', 'you', 'with', 'towards', 'make', 'really', 'few', 'former', 
+        'during', 'mine', 'do', 'would', 'of', 'off', 'six', 'yourself', 'becoming', 'through', 
+        'seeming', 'hence', 'us', 'anywhere', 'regarding', 'whole', 'down', 'seem', 'whereas', 'to', 
+        'their', 'various', 'thereafter', 'â€˜d', 'above', 'put', 'sometime', 'moreover', 'whoever', 'although', 
+        'at', 'four', 'each', 'among', 'whatever', 'any', 'anyhow', 'herein', 'become', 'last', 'between', 'still', 
+        'was', 'almost', 'twelve', 'used', 'who', 'go', 'not', 'enough', 'well', 'â€™ve', 'might', 'see', 'whose', 
+        'everywhere', 'yourselves', 'across', 'myself', 'further', 'did', 'then', 'is', 'except', 'up', 'take', 
+        'became', 'however', 'many', 'thence', 'onto', 'â€˜m', 'my', 'own', 'must', 'wherein', 'elsewhere', 'behind', 
+        'becomes', 'alone', 'due', 'being', 'neither', 'a', 'over', 'beside', 'fifteen', 'meanwhile', 'upon', 'next', 
+        'forty', 'what', 'less', 'and', 'please', 'toward', 'about', 'below', 'hereafter', 'whether', 'yet', 'nor', 
+        'against', 'whereupon', 'top', 'first', 'three', 'show', 'per', 'five', 'two', 'ourselves', 'whenever', 
+        'get', 'thereby', 'noone', 'had', 'now', 'everyone', 'everything', 'nowhere', 'ca', 'though', 'least', 
+        'so', 'both', 'otherwise', 'whereby', 'unless', 'somewhere', 'give', 'formerly', 'â€™d', 'under', 
+        'while', 'empty', 'doing', 'besides', 'thus', 'this', 'anyone', 'its', 'after', 'bottom', 'call', 
+        'nâ€™t', 'name', 'even', 'eleven', 'by', 'from', 'when', 'or', 'anyway', 'how', 'the', 'all', 
+        'much', 'another', 'since', 'hundred', 'serious', 'â€˜ve', 'ever', 'out', 'full', 'themselves', 
+        'been', 'in', "'d", 'wherever', 'part', 'someone', 'therein', 'can', 'seemed', 'hereby', 'others', 
+        "'s", "'re", 'most', 'one', "n't", 'into', 'some', 'will', 'these', 'twenty', 'here', 'as', 'nobody', 
+        'also', 'along', 'than', 'anything', 'he', 'there', 'does', 'we', 'â€™ll', 'latterly', 'are', 'ten', 
+        'hers', 'should', 'they', 'â€˜s', 'either', 'am', 'be', 'perhaps', 'â€™re', 'only', 'namely', 'sixty', 
+        'made', "'m", 'always', 'those', 'have', 'again', 'her', 'once', 'ours', 'herself', 'else', 'has', 'nine', 
+        'more', 'sometimes', 'your', 'yours', 'that', 'around', 'his', 'indeed', 'mostly', 'cannot', 'â€˜ll', 'too', 
+        'seems', 'â€™m', 'himself', 'latter', 'whither', 'amount', 'other', 'nevertheless', 'whom', 'for', 'somehow', 
+        'beforehand', 'just', 'an', 'beyond', 'amongst', 'none', "'ve", 'say', 'via', 'but', 'often', 're', 'our', 
+        'because', 'rather', 'using', 'without', 'throughout', 'on', 'she', 'never', 'eight', 'no', 'hereupon', 
+        'them', 'whereafter', 'quite', 'which', 'move', 'thru', 'until', 'afterwards', 'fifty', 'i', 'itself', 'nâ€˜t',
+        'him', 'could', 'front', 'within', 'â€˜re', 'back', 'such', 'already', 'several', 'side', 'whence', 'me', 
+        'same', 'were', 'it', 'every', 'third', 'together'
+    ]
+    }
+
     # Create 'Without Stop Words' column: Mikael
     df['Without Stop Words'] = df['Tweets'].str.lower().str.split()
 
     # Extract stop words from 'Without Stop Words' column: Monica
-    pass
+    for row in df['Without Stop Words']:
+        for word in row:
+        #find stop word in stop word dictionary
+            for stop_word in stop_words_dict['stopwords']:
+                if word == stop_word:
+                    #remove stop word from current row
+                    row.remove(word)
+    return df
